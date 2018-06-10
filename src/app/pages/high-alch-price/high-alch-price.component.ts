29f174e7d7fd750b3cacc61ba4f2290b57bf154a
fredrik62 <hogstrom10@gmail.com>
2018-06-10 05:55:30 +0200

    fixed high-alch-price logic

--- src/app/pages/high-alch-price/high-alch-price.component.ts
+++ src/app/pages/high-alch-price/high-alch-price.component.ts
@@ -20,54 +20,61 @@ export class HighAlchPriceComponent implements OnInit {
   constructor(private grandExchangeService: GrandExchangeService, private router: Router) {}
   
   ngOnInit() {
-      this.grandExchangeService.getAllItems()
-          .toPromise()
-          .then((res) => {
-                  this.data = res;
-  
-  
-                  Object.keys(this.data).forEach(key => {
-                 
+    this.grandExchangeService.getAllItems()
+    .toPromise()
+    .then((res) => {
+     this.data = res;
+     //if no nature rune price index has changed
+     this.natPrice = this.data[561].sell_average;
+     
+     Object.keys(this.data).forEach(key => {
+       this.geData = {
+           id: this.data[key].id,
+           name: this.data[key].name,
+           buyAverage: this.data[key].buy_average,
+           sellAverage: this.data[key].sell_average,
+           overAllAverage: this.data[key].overall_average,
+           alchPrice: this.data[key].sp * 0.6,
+           natureRuneCost: this.natPrice,
+           total: this.natPrice + this.data[key].sell_average,
+           lossOrProfit: this.data[key].sp * 0.6 - (this.natPrice + this.data[key].sell_average)
 
-                    if (this.data[key].name === 'Nature rune') {
-                      this.natPrice = this.data[key].buy_average;
-                    }
-                      this.geData = {
-                          id: this.data[key].id,
-                          name: this.data[key].name,
-                          buyAverage: this.data[key].buy_average,
-                          sellAverage: this.data[key].sell_average,
-                          overAllAverage: this.data[key].overall_average,
-                          alchPrice: this.data[key].sp * 0.6,
-                         
+
+       }
+       if (this.geData.alchPrice > 300 && this.geData.total !== this.geData.natureRuneCost) {
+          this.geItemArray.push(this.geData);
+          this.geItemArray.sort(( a, b )  => {
+            return b.lossOrProfit - a.lossOrProfit;
+          });
+          
+
+        } 
+      
+      });
+      
+    })
+    }
+       
+    displayItemData(id) {
+    this.router.navigate(['high-alch-calculator-2007-osrs', id]);
+    }     
+}
+            
   
   
-                      }
+
+                  
+          
+                  
+
+
                      
                       
-                      
-                      
-                      this.geItemArray.push(this.geData);
-                    //   console.log(this.geData.alchPrice - (this.geData.buyAverage + this.natPrice))
-
   
-  
-                  });
-  
-  
-                }
-                
-                
-            )
-        }
-                        displayItemData(id) {
-                            this.router.navigate(['high-alch-calculator-2007-osrs', id]);
-                        }
-  }
 //TODO UPDATE OBJECT INSTEAD OF CALCULATING IT IN THE HTML
 // 1. filter out items if they are less than the nature rune price
 // 2. update object and store the nature price in the object
 // 3. red or green background colors depending if profit or loss
-// 4. page pagination limit results to 50 per page
-// 5. add a search bar to filter results
+// 4. done
+// 5. done
 // 6. chartz
