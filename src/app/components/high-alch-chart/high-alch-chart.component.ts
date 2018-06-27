e0dcfbd18ddf385c9275a3b83cd36ed57faf3a32
fredrik62 <hogstrom10@gmail.com>
2018-06-27 22:41:46 +0300

    more css

--- src/app/components/high-alch-chart/high-alch-chart.component.ts
+++ src/app/components/high-alch-chart/high-alch-chart.component.ts
@@ -14,91 +14,91 @@ import { GrandExchangeService } from '../../services/grand-exchange.service';
 export class HighAlchChartComponent implements OnInit {
 data: any;
 itemInfo: any;
+
+item: any = []
 graphInfo: any;
 graph: any;
 daily: any;
 average: any;
 dailyTime180: any;
-thirtyDayChange: any;
-ninetyDayChange: any;
-hundredEightyDayChange: any;
+thirtyDayChange: Number;
+ninetyDayChange: Number;
+hundredEightyDayChange: Number;
+thirty: boolean = false;
+ninenty: boolean = false;
+hundredEighty: boolean = false;
+
 show30: boolean;
 show90: boolean;
 show180: boolean;
   constructor(private activatedRoute: ActivatedRoute, private grandExchangeService: GrandExchangeService) { }
 
-  ngOnInit() {
-    this.activatedRoute.params.subscribe((params) => {
-      const id = params.id;
-        this.grandExchangeService.getOneItem(id)
-         .toPromise()
-           .then((res) => {
-            this.data = res;
-            
-             
-             this.itemInfo = {
-              itemName: this.data.itemData.item.name,
-              smallIcon: this.data.itemData.item.icon,
-              largeIcon: this.data.itemData.item.icon_large,
-              itemDescription: this.data.itemData.item.description,
-              current: this.data.itemData.item.current.price,
-              day30: this.data.itemData.item.day30.change,
-              day90: this.data.itemData.item.day90.change,
-              day180: this.data.itemData.item.day180.change
-                    }
-                    
-                    
-                   console.log(this.itemInfo.smallIcon);
+ngOnInit() {
+this.activatedRoute.params.subscribe((params) => {
+const id = params.id;
+this.grandExchangeService.getOneItem(id)
+.toPromise()
+.then((res) => {
+this.data = res;
 
-                    //positive or negative trends on pagge
-                     this.thirtyDayChange = parseInt(this.itemInfo.day30);
-                     this.ninetyDayChange = parseInt(this.itemInfo.day90);
-                     this.hundredEightyDayChange = parseInt(this.itemInfo.day180);
-                     
-                     if (this.thirtyDayChange < 0) {
-                       this.thirtyDayChange.negative = true;
-                     }
+this.itemInfo = {
+name: this.data.itemData.item.name,
+smallIcon: this.data.itemData.item.icon,
+largeIcon: this.data.itemData.item.icon_large,
+itemDescription: this.data.itemData.item.description,
+current: this.data.itemData.item.current.price,
+day30: this.data.itemData.item.day30.change,
+day90: this.data.itemData.item.day90.change,
+day180: this.data.itemData.item.day180.change
+}
+this.item.push(this.itemInfo);
 
-                     if (this.ninetyDayChange < 0) {
-                      this.ninetyDayChange.negative = true;
-                    }
-                    if (this.hundredEightyDayChange < 0) {
-                      this.hundredEightyDayChange.negative = true;
-                    }
-              })
-            })
-            
-          }
-          //buttons to show which graphs to display
-           showDays(numOfDays) {
-            this.show30 = true;
-            this.show90 = false;
-            this.show180 = false;
 
-            switch(numOfDays) {
-                case 30:
-                this.show30 = false;
-                break;
-                case 90:
-                this.show90 = true;
-                break;
-                case 180:
-                this.show180 = true;
-                break;
-              }
+
+//parse the percentage
+this.thirtyDayChange = parseInt(this.itemInfo.day30);
+this.ninetyDayChange = parseInt(this.itemInfo.day90);
+this.hundredEightyDayChange = parseInt(this.itemInfo.day180);
+
+if (this.thirtyDayChange < 0) {
+this.thirty = true;
+}
+
+if (this.ninetyDayChange < 0) {
+this.ninenty = true;
+}
+
+if (this.hundredEightyDayChange < 0) {
+this.hundredEighty = true;
+}
+
+})
+})
+
+}
+//buttons to show which graphs to display
+showDays(numOfDays) {
+this.show30 = true;
+this.show90 = false;
+this.show180 = false;
+
+switch(numOfDays) {
+case 30:
+this.show30 = false;
+break;
+case 90:
+this.show90 = true;
+break;
+case 180:
+this.show180 = true;
+break;
+}
+
+
+}
+
+
+
+}
+
               
-
-          }
-        
-          
-      
-        }
-                        
-                
-
-
-
-// THEN MAKE GRAPH
-// 30 DAY
-// 90 DAY
-// 180 DAY
--- src/app/pages/high-alch-price/high-alch-price.component.ts
+++ src/app/pages/high-alch-price/high-alch-price.component.ts
@@ -21,76 +21,76 @@ export class HighAlchPriceComponent implements OnInit {
 
   constructor(private grandExchangeService: GrandExchangeService, private router: Router) {}
   
-  ngOnInit() {
-    this.grandExchangeService.getAllItems()
-    .toPromise()
-    .then((res) => {
-     this.data = res;
-     this.iconUrl = 'http://services.runescape.com/m=itemdb_oldschool/1529576970247_obj_sprite.gif?id=';  
-     //if no nature rune price, index has changed
-     this.natPrice = this.data[561].sell_average;
-     
-     Object.keys(this.data).forEach(key => {
-       this.geData = {
-           id: this.data[key].id,
-           image: this.iconUrl + this.data[key].id,
-           name: this.data[key].name,
-           buyAverage: this.data[key].buy_average,
-           sellAverage: this.data[key].sell_average,
-           overAllAverage: this.data[key].overall_average,
-           alchPrice: this.data[key].sp * 0.6,
-           natureRuneCost: this.natPrice,
-           total: this.natPrice + this.data[key].sell_average,
-           lossOrProfit: this.data[key].sp * 0.6 - (this.natPrice + this.data[key].sell_average),
+ngOnInit() {
+this.grandExchangeService.getAllItems()
+.toPromise()
+.then((res) => {
+this.data = res;
+this.iconUrl = 'http://services.runescape.com/m=itemdb_oldschool/1529576970247_obj_sprite.gif?id=';  
+//if no nature rune price, index has changed
+this.natPrice = this.data[561].sell_average;
+
+Object.keys(this.data).forEach(key => {
+this.geData = {
+id: this.data[key].id,
+image: this.iconUrl + this.data[key].id,
+name: this.data[key].name,
+buyAverage: this.data[key].buy_average,
+sellAverage: this.data[key].sell_average,
+overAllAverage: this.data[key].overall_average,
+alchPrice: this.data[key].sp * 0.6,
+natureRuneCost: this.natPrice,
+total: this.natPrice + this.data[key].sell_average,
+lossOrProfit: this.data[key].sp * 0.6 - (this.natPrice + this.data[key].sell_average),
 
 
-       }
-       if (this.geData.alchPrice > 300 && this.geData.total !== this.geData.natureRuneCost) {
-          this.geItemArray.push(this.geData);
-          this.geItemArray.sort(( a, b )  => {
-            return b.lossOrProfit - a.lossOrProfit;
-          });
-          
-         
-        }
-        if (this.geData.lossOrProfit < 0) {
-           this.geData.negativeChange = true;
-        } 
-      
-      });
-
-      
-    })
-    }
-
-    reverseOrder() {
-      this.geItemArray.sort(( a, b )  => {
-        return a.lossOrProfit - b.lossOrProfit;
-      });
-      this.reverseCount++;
-      if (this.reverseCount === 2) {
-        this.geItemArray.sort(( a, b )  => {
-          return b.lossOrProfit - a.lossOrProfit;
-        });
-        this.reverseCount = 0;
-      }
-     }
-       
-    displayItemData(id) {
-    this.router.navigate(['high-alch-calculator-2007-osrs', id]);
-    }     
 }
-            
-  
-  
-
-                  
-          
-                  
+if (this.geData.alchPrice > 300 && this.geData.total !== this.geData.natureRuneCost) {
+this.geItemArray.push(this.geData);
+this.geItemArray.sort(( a, b )  => {
+return b.lossOrProfit - a.lossOrProfit;
+});
 
 
-                     
-                      
+}
+if (this.geData.lossOrProfit < 0) {
+this.geData.negativeChange = true;
+} 
+
+});
+
+
+})
+}
+
+reverseOrder() {
+this.geItemArray.sort(( a, b )  => {
+return a.lossOrProfit - b.lossOrProfit;
+});
+this.reverseCount++;
+if (this.reverseCount === 2) {
+this.geItemArray.sort(( a, b )  => {
+return b.lossOrProfit - a.lossOrProfit;
+});
+this.reverseCount = 0;
+}
+}
+
+displayItemData(id) {
+this.router.navigate(['high-alch-calculator-2007-osrs', id]);
+}     
+}
+
+
+
+
+    
+
+    
+
+
+        
+        
   
 //done
 // 1. done
