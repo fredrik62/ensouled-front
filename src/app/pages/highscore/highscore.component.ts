9af6577034f9a00cfa6a5ae2279b66e032cff8c6
fredrik62 <hogstrom10@gmail.com>
2018-07-08 21:43:43 +0300

    display player stats

--- src/app/pages/highscore/highscore.component.ts
+++ src/app/pages/highscore/highscore.component.ts
@@ -7,14 +7,24 @@ import { GetPlayersService } from '../../services/get-players.service';
   styleUrls: ['./highscore.component.css']
 })
 export class HighscoreComponent implements OnInit {
-
+players: any = [];
   constructor(private getPlayersService: GetPlayersService) { }
 
   ngOnInit() {
     this.getPlayersService.getAllPlayer()
     .toPromise()
     .then((res) => {
-      console.log(res);
+     
+    
+     for (var p in res) {
+       let accountData = {
+       playerName: res[p].username,
+       overAllRank: res[p].overAllRank,
+       totalExperience: res[p].totalExperience,
+       totalLevel:res[p].totalLevel
+      } 
+     this.players.push(accountData)
+     }
     })
   }
 
