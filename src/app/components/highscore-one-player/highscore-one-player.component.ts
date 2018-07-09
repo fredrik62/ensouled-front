853e78d16cff4023fb6ba749809a79dff18223f6
fredrik62 <hogstrom10@gmail.com>
2018-07-09 15:07:34 +0300

    skill badges done

--- src/app/components/highscore-one-player/highscore-one-player.component.ts
+++ src/app/components/highscore-one-player/highscore-one-player.component.ts
@@ -10,14 +10,19 @@ import { ActivatedRoute } from '@angular/router';
 export class HighscoreOnePlayerComponent implements OnInit {
 rsn: String;
 playerStats: any = [];
+skillBadges = ['Attack_icon.png', 'Defence_icon.png', 'Strength_icon.png', 'Ranged_icon.png', 'Magic_icon.png', 'Cooking_icon.png'
+,'Woodcutting_icon.png', 'Fletching_icon.png', 'Fishing_icon.png', 'Firemaking_icon.png', 'Crafting_icon.png'
+,'Smithing_icon.png', 'Mining_icon.png', 'Herblore_icon.png', 'Agility_icon.png', 'Thieving_icon.png', 'Slayer_icon.png'
+,'Farming_icon.png', 'Runecrafting_icon.png', 'Hunter_icon.png', 'Construction_icon.png'];
   constructor(private playerLookupService: PlayerLookupService, private activatedRoute: ActivatedRoute) { }
-
+  
   ngOnInit() {
     this.activatedRoute.params.subscribe((get) => {
       this.rsn = get.player;
       this.playerLookupService.getOnePlayer(this.rsn)
      .toPromise()
      .then((result) => {
+     
       for (var key in result) {
         if (result.hasOwnProperty(key)) {
             let playerData = {
@@ -25,8 +30,14 @@ playerStats: any = [];
               rank: result[key].rank,
               level: result[key].level,
               xp: result[key].xp,
+              
             }
-            this.playerStats.push(playerData);
+            if (playerData.rank !== undefined) {
+              this.playerStats.push(playerData);
+              
+            }
+            
+           
         }
     }
       
